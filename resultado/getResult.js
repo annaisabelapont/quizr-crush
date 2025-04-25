8ec37dea2b2038eb7c6fbea22f99623a06fed752
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-04-25 10:43:19 -0400

    feat: add arthur in options

--- resultado/getResult.js
+++ resultado/getResult.js
@@ -1,80 +1,86 @@
 export function getResult(personName) {
-  switch(personName.toLowerCase()) {
-    case 'anna isabela':
-    case 'annisabolas':
+  switch (personName.toLowerCase()) {
+    case "anna isabela":
+    case "annisabolas":
       return {
-        love: 'Elefante do PHP',
-        imgSrc: 'elefantePHP.png'
+        love: "Elefante do PHP",
+        imgSrc: "elefantePHP.png",
       };
 
-    case 'dhuli':
+    case "dhuli":
       return {
-        love: 'Cachorrinho da figurinha',
-        imgSrc: 'cachorrinhoFig.png'
+        love: "Cachorrinho da figurinha",
+        imgSrc: "cachorrinhoFig.png",
       };
 
-    case 'giovanna':
-    case 'giovanna rodrigues':
+    case "giovanna":
+    case "giovanna rodrigues":
       return {
-        love: 'Bonnie',
-        imgSrc: 'bonnie.png'
+        love: "Bonnie",
+        imgSrc: "bonnie.png",
       };
 
-    case 'karan':
-    case 'professor karan':
-    case 'karan luciano':
+    case "karan":
+    case "professor karan":
+    case "karan luciano":
       return {
-        love: 'Shrek jogador',
-        imgSrc: 'shrekJogador.png'
+        love: "Shrek jogador",
+        imgSrc: "shrekJogador.png",
       };
 
-    case 'emanueli':
-    case 'manu':
+    case "emanueli":
+    case "manu":
       return {
-        love: 'Charles Leclerc',
-        imgSrc: 'charlesLeclerc.png'
+        love: "Charles Leclerc",
+        imgSrc: "charlesLeclerc.png",
       };
 
-    case 'luiza':
-    case 'maria luiza':
-    case 'luiza souza':
+    case "luiza":
+    case "maria luiza":
+    case "luiza souza":
       return {
-        love: 'Caminhoneiro',
-        imgSrc: 'patriota.png'
+        love: "Caminhoneiro",
+        imgSrc: "patriota.png",
       };
 
-    case 'lucas':
-    case 'lucas oliveira':
+    case "lucas":
+    case "lucas oliveira":
       return {
-        love: 'Nikolas Ferreira',
-        imgSrc: 'nikolasFerreira.png'
+        love: "Nikolas Ferreira",
+        imgSrc: "nikolasFerreira.png",
       };
 
-    case 'luis miguel':
-    case 'luis':
+    case "luis miguel":
+    case "luis":
       return {
-        love: 'Pabllo Vittar',
-        imgSrc: 'pablloVittar.png'
+        love: "Pabllo Vittar",
+        imgSrc: "pablloVittar.png",
       };
 
-    case 'giovana':
-    case 'giovana santana':
+    case "giovana":
+    case "giovana santana":
       return {
-        love: 'Jimin',
-        imgSrc: 'jimin.png'
+        love: "Jimin",
+        imgSrc: "jimin.png",
       };
 
-    case 'mariana':
-    case 'mariana da luz':
+    case "mariana":
+    case "mariana da luz":
       return {
-        love: 'Padre Kelmon',
-        imgSrc: 'padreKelmon.png'
+        love: "Padre Kelmon",
+        imgSrc: "padreKelmon.png",
+      };
+
+    case "arthur":
+      return {
+        love: "Freddie Mercury",
+        imgSrc: "freddieMercury.png",
       };
 
     default:
       return {
-        love: 'Tralalelo Tralala',
-        imgSrc: 'tralaleloTralala.png'
+        love: "Tralalelo Tralala",
+        imgSrc: "tralaleloTralala.png",
       };
   }
 }
